cd81a33acd2eb589bcb71bbbec5226ea29a6a4eb
yNickFtw <nkxynickftw@gmail.com>
2023-01-27 22:45:14 -0300

    db conn

--- db/conn.js
+++ db/conn.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(process.env.DBNAME || 'toughts', process.env.DBUSER || 'root', process.env.DBPASS || '', {
     host: process.env.DBHOST || 'localhost',
     dialect: 'mysql',
+    port: process.env.DBPORT || 3306
 })
 
 try {
